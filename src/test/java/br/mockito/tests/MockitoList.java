e368a37d4f3e7100629a410112d7c53f5991d2ec
Adriano Avelino <adriano-st777@hotmail.com>
2020-05-20 10:24:29 -0300

    adiciona testes usando verify e exception

--- src/test/java/br/mockito/tests/MockitoList.java
+++ src/test/java/br/mockito/tests/MockitoList.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Test;
@@ -19,7 +20,29 @@ public class MockitoList {
 	@Test
 	public void adicionaUmaItem() {
 		List<String> list = mock(List.class);
-	    when(list.size()).thenReturn(1);
-	    assertEquals(1, list.size());
+		when(list.size()).thenReturn(1);
+		assertEquals(1, list.size());
+	}
+	
+	@Test
+	public void adicionaItemUsandoVerify() {
+		List<String> mockedList = mock(List.class);
+
+		mockedList.add("one");
+		verify(mockedList).add("one");
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void usando() {
+		LinkedList mockedList = mock(LinkedList.class);
+
+		when(mockedList.get(0)).thenReturn("first");
+		when(mockedList.get(1)).thenThrow(new RuntimeException());
+
+		System.out.println(mockedList.get(0));
+		System.out.println(mockedList.get(1));
+		System.out.println(mockedList.get(2));
+		verify(mockedList).get(0);
+		verify(mockedList).get(1);
 	}
 }
